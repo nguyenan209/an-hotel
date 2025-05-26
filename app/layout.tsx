b6e0e28acc145f93f44e0a855acfbdbc080a749a
anntl <anntl.work@gmail.com>
2025-05-27 06:04:32 +0700

    feat: add some meta header

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ConditionalFooter } from "@/components/layout/conditional-footer";
 import CustomQueryClientProvider from "@/components/providers/CustomQueryClientProvider";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "sonner";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi">
+      <Head>
+        <title>Homestay Booking - Tìm và đặt homestay tuyệt vời</title>
+        <meta
+          name="description"
+          content="Cùng An khám phá những homestay phù hợp với bạn. Tìm kiếm và đặt phòng homestay dễ dàng và nhanh chóng."
+        />
+        <meta
+          property="og:title"
+          content="An's Homestay - Tìm và đặt homestay tuyệt vời"
+        />
+        <meta
+          property="og:description"
+          content="Cùng An khám phá những homestay phù hợp với bạn. Tìm kiếm và đặt phòng homestay dễ dàng và nhanh chóng."
+        />
+        <meta property="og:image" content="/images/sunset-beach-villa-2.png" />
+        <meta
+          property="og:url"
+          content={`${process.env.NEXT_PUBLIC_API_URL}/`}
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="An's Homestay - Tìm và đặt homestay tuyệt vời"
+        />
+        <meta
+          name="twitter:description"
+          content="Cùng An khám phá những homestay phù hợp với bạn. Tìm kiếm và đặt phòng homestay dễ dàng và nhanh chóng."
+        />
+        <meta name="twitter:image" content="/images/sunset-beach-villa-2.png" />
+      </Head>
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col">
           <AuthProvider>
