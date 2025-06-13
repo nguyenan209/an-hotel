346721b73589e05230649dde5e61802fa4ea8f04
anntl <anntl.work@gmail.com>
2025-06-13 10:36:43 +0700

    fix: type error

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -50,8 +50,8 @@ export async function POST(req: Request) {
 export async function GET() {
   const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
   const options = {
-    redirect_uri: process.env.GOOGLE_REDIRECT_URI,
-    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: process.env.GOOGLE_REDIRECT_URI || "",
+    client_id: process.env.GOOGLE_CLIENT_ID || "",
     access_type: "offline",
     response_type: "code",
     prompt: "consent",
