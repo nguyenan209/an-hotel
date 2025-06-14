7a83f2f41514da7611865885d4adf72c93b8cfc9
anntl <anntl.work@gmail.com>
2025-06-14 10:04:16 +0700

    fix: background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ConditionalFooter } from "@/components/layout/conditional-footer";
 import CustomQueryClientProvider from "@/components/providers/CustomQueryClientProvider";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "sonner";
-import Head from "next/head";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,50 +23,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="vi">
-      <Head>
-        <title>Homestay Booking - Tìm và đặt homestay tuyệt vời</title>
-        <meta
-          name="description"
-          content="Cùng An khám phá những homestay phù hợp với bạn. Tìm kiếm và đặt phòng homestay dễ dàng và nhanh chóng."
-        />
-        <meta
-          property="og:title"
-          content="An's Homestay - Tìm và đặt homestay tuyệt vời"
-        />
-        <meta
-          property="og:description"
-          content="Cùng An khám phá những homestay phù hợp với bạn. Tìm kiếm và đặt phòng homestay dễ dàng và nhanh chóng."
-        />
-        <meta property="og:image" content="/images/sunset-beach-villa-2.png" />
-        <meta
-          property="og:url"
-          content={`${process.env.NEXT_PUBLIC_API_URL}/`}
-        />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="An's Homestay - Tìm và đặt homestay tuyệt vời"
-        />
-        <meta
-          name="twitter:description"
-          content="Cùng An khám phá những homestay phù hợp với bạn. Tìm kiếm và đặt phòng homestay dễ dàng và nhanh chóng."
-        />
-        <meta name="twitter:image" content="/images/sunset-beach-villa-2.png" />
-        <link rel="icon" href="/logo.png" />
-      </Head>
-      <body className={inter.className}>
-        <div className="flex min-h-screen flex-col">
+    <html lang="vi" suppressHydrationWarning>
+      <body className={`${inter.className} min-h-screen bg-gradient-to-r from-[#EFE4D2]/20 to-[#EFE4D2]/30`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <AuthProvider>
             <ConditionalHeader />
             <CustomQueryClientProvider>
-              <main className="flex-1 bg-gray-50">{children}</main>
+              <main className="flex-1">{children}</main>
             </CustomQueryClientProvider>
             <ConditionalFooter />
           </AuthProvider>
           <Toaster position="top-center" richColors />
-        </div>
+        </ThemeProvider>
       </body>
     </html>
   );
